7133b1a9295a216b580597f56e97f97151e8d200
vagrant <vagrant@homestead>
2020-10-20 02:26:40 +0000

    限制大頭貼分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -38,6 +39,8 @@ class UserRequest extends FormRequest
             'name.regex' => '用戶名只支持英文、數字、橫槓或下划線。',
             'name.between' => '用戶名必須介於 3 - 25 個字符之間。',
             'name.required' => '用戶名不能為空。',
+            'avatar.mimes' =>'頭像必須是 jpeg, bmp, png, gif 格式的圖片',
+            'avatar.dimensions' => '圖片的清晰度不够，寬和高需要 208px 以上',
         ];
     }
 }
